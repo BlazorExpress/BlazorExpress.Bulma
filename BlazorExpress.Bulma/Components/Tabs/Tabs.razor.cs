20edcc65a1c0d854e8c4e57e2afe35426687f1c0
Vikram Reddy <gvreddy04@gmail.com>
2024-07-26 11:17:37 +0530

    Removed unused .css files. Tabs component related files added.

---
+++ BlazorExpress.Bulma/Components/Tabs/Tabs.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorExpress.Bulma;
+
+/// <summary>
+/// <see href="https://bulma.io/documentation/components/tabs/" />
+/// </summary>
+public partial class Tabs : BulmaComponentBase
+{
+    #region Properties, Indexers
+
+    protected override string? CssClassNames
+        => CssUtility.BuildClassNames(Class, (BulmaCssClass.Block, true));
+
+    /// <summary>
+    /// Gets or sets the child content.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="null"/>.
+    /// </remarks>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    #endregion
+}
