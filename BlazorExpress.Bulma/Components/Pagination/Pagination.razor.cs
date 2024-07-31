818cd53694c3f2488a0b6a0ad756f7ed9ee5257d
Vikram Reddy <gvreddy04@gmail.com>
2024-07-31 22:42:20 +0530

    Pagination component updated

--- BlazorExpress.Bulma/Components/Pagination/Pagination.razor.cs
+++ BlazorExpress.Bulma/Components/Pagination/Pagination.razor.cs
@@ -1,6 +1,7 @@
 ﻿namespace BlazorExpress.Bulma;
 
 /// <summary>
+/// Pagination component
 /// <see href="https://bulma.io/documentation/components/pagination/" />
 /// </summary>
 public partial class Pagination : BulmaComponentBase
@@ -18,17 +19,79 @@ public partial class Pagination : BulmaComponentBase
         base.OnInitialized();
     }
 
+    private int GetPageFromInclusive()
+    {
+        var q = ActivePageNumber / DisplayPages;
+        var r = ActivePageNumber % DisplayPages;
+
+        if (q < 1)
+            return 1;
+
+        if (q > 0 && r == 0)
+            return (q - 1) * DisplayPages + 1;
+
+        if (q > 1 && r < DisplayPages)
+            return q * DisplayPages + 1;
+
+        return ActivePageNumber / DisplayPages * DisplayPages + 1;
+    }
+
+    private int GetPageToExclusive() => TotalPages == 0 ? 1 : Math.Min(TotalPages, PageFromInclusive + DisplayPages - 1);
+
+    private void GoToNextPage()
+    {
+        if (ActivePageNumber == TotalPages || TotalPages == 0 || TotalPages == 1)
+            return;
+
+        ActivePageNumber += 1;
+
+        if (PageChanged.HasDelegate)
+            PageChanged.InvokeAsync(ActivePageNumber);
+    }
+
+    private void GoToPage(int pageNumber)
+    {
+        if (pageNumber < 1 || pageNumber > TotalPages)
+            return;
+
+        ActivePageNumber = pageNumber;
+
+        if (PageChanged.HasDelegate)
+            PageChanged.InvokeAsync(ActivePageNumber);
+    }
+
+    private void GoToPreviousPage()
+    {
+        if (ActivePageNumber <= 1)
+            return;
+
+        ActivePageNumber -= 1;
+
+        if (PageChanged.HasDelegate)
+            PageChanged.InvokeAsync(ActivePageNumber);
+    }
+
     #endregion
 
     #region Properties, Indexers
 
-    protected override string? CssClassNames
-        => CssUtility.BuildClassNames(
-            Class, 
+    protected override string? CssClassNames =>
+        CssUtility.BuildClassNames(
+            Class,
             (BulmaCssClass.Pagination, true),
             (Alignment.ToPaginationAlignmentClass(), Alignment != PaginationAlignment.None),
             (BulmaCssClass.IsRounded, IsRounded),
-            (Size.ToPaginationSizeClass(), Size != PaginationSize.None));
+            (Size.ToPaginationSizeClass(), Size != PaginationSize.None)
+        );
+
+    /// <summary>
+    /// Gets or sets the active page number.
+    /// </summary>
+    /// <remarks>
+    /// Default value is 1.
+    /// </remarks>
+    [Parameter]
+    public int ActivePageNumber { get; set; } = 1;
 
     /// <summary>
     /// Get or sets the <see cref="Pagination" /> alignment.
@@ -40,13 +103,36 @@ public partial class Pagination : BulmaComponentBase
     public PaginationAlignment Alignment { get; set; }
 
     /// <summary>
-    /// When set to <see langword="true"/>, changes the appearance of <see cref="PaginationItem"/> to rounded.
+    /// Gets or sets the maximum page links to be displayed.
     /// </summary>
     /// <remarks>
-    /// The default value is <see langword="false"/>.
+    /// Default value is 5.
+    /// </remarks>
+    [Parameter]
+    public int DisplayPages { get; set; } = 5;
+
+    /// <summary>
+    /// When set to <see langword="true" />, changes the appearance of <see cref="PaginationItem" /> to rounded.
+    /// </summary>
+    /// <remarks>
+    /// The default value is <see langword="false" />.
     /// </remarks>
     [Parameter]
     public bool IsRounded { get; set; }
+
+    [Parameter] public RenderFragment? NextButtonTemplate { get; set; }
+
+    /// <summary>
+    /// This event fires immediately when the page number is changed.
+    /// </summary>
+    [Parameter]
+    public EventCallback<int> PageChanged { get; set; }
+
+    private int PageFromInclusive => GetPageFromInclusive();
+
+    private int PageToExclusive => GetPageToExclusive();
+
+    [Parameter] public RenderFragment? PreviousButtonTemplate { get; set; }
 
     /// <summary>
     /// Get or sets the <see cref="Pagination" /> size.
@@ -56,6 +142,15 @@ public partial class Pagination : BulmaComponentBase
     /// </remarks>
     [Parameter]
     public PaginationSize Size { get; set; }
+
+    /// <summary>
+    /// Gets or sets the total pages.
+    /// </summary>
+    /// <remarks>
+    /// Default value is 0.
+    /// </remarks>
+    [Parameter]
+    public int TotalPages { get; set; }
 
     #endregion
 }
---
+++ BlazorExpress.Bulma/Components/Pagination/PaginationItem.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace BlazorExpress.Bulma;
+
+public partial class PaginationItem : BulmaComponentBase
+{
+    #region Properties, Indexers
+
+    protected override string? CssClassNames =>
+        CssUtility.BuildClassNames(
+            Class,
+            (Type.ToPaginationLinkTypeClass(), true),
+            (BulmaCssClass.IsCurrent, IsCurrentPage),
+            (BulmaCssClass.IsDisabled, IsDisabled)
+        );
+
+    /// <summary>
+    /// Gets or sets the child content.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="null" />.
+    /// </remarks>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// If <see langword="true" />, sets the <see cref="PaginationItem"/> as current page.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="false" />.
+    /// </remarks>
+    [Parameter] public bool IsCurrentPage { get; set; }
+
+    /// <summary>
+    /// If <see langword="true" />, disables the <see cref="PaginationItem"/>.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="false" />.
+    /// </remarks>
+    [Parameter] public bool IsDisabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets the  <see cref="PaginationItem" /> type.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see cref="PaginationLinkType.None" />.
+    /// </remarks>
+    [Parameter]
+    public PaginationLinkType Type { get; set; } = PaginationLinkType.None;
+
+    #endregion
+}
