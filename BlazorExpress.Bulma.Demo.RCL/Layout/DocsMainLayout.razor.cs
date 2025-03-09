e18b2e6015064ec7d96527928f4f237ab16f9b5e
Vikram Reddy <gvreddy04@gmail.com>
2025-03-09 22:58:05 +0530

    ConfirmDialog - Initial setup

--- BlazorExpress.Bulma.Demo.RCL/Layout/DocsMainLayout.razor.cs
+++ BlazorExpress.Bulma.Demo.RCL/Layout/DocsMainLayout.razor.cs
@@ -86,16 +86,16 @@ public partial class DocsMainLayout : MainLayoutBase
             Name = "COMPONENTS",
             CssClass = "is-size-7 has-text-weight-bold has-text-dark",
             Links = [
-                new Link { Href = RouteConstants.Docs_Message_Documentation, Text = "Message" },
+                new Link { Href = RouteConstants.Docs_ConfirmDialog_Documentation, Text = "Confirm Dialog" },
                 new Link { Href = RouteConstants.Docs_GoogleMaps_Documentation, Text = "Google Maps" },
                 new Link { Href = RouteConstants.Docs_Grid_Documentation, Text = "Grid" },
+                new Link { Href = RouteConstants.Docs_Message_Documentation, Text = "Message" },
                 new Link { Href = RouteConstants.Docs_Modal_Documentation, Text = "Modal" },
                 new Link { Href = RouteConstants.Docs_Pagination_Documentation, Text = "Pagination" },
                 new Link { Href = RouteConstants.Docs_ScriptLoader_Documentation, Text = "Script Loader" },
                 new Link { Href = RouteConstants.Docs_Tabs_Documentation, Text = "Tabs" }
             ]
-        });
-        
+        });        
 
         // LAYOUT
         groups.Add(new LinkGroup
---
+++ BlazorExpress.Bulma/Components/ConfirmDialog/ConfirmDialog.razor.cs
@@ -0,0 +1,120 @@
+﻿namespace BlazorExpress.Bulma;
+
+/// <summary>
+/// ConfirmDialog component
+/// <para>
+/// <see href="https://bulma.io/documentation/components/modal/" />
+/// </para>
+/// </summary>
+public partial class ConfirmDialog : BulmaComponentBase
+{
+    private string? title = null;
+    private string? titleHtml = null;
+    private string? message = null;
+    private string? messageHtml = null;
+    private string yesButtonText { get; set; } = "Yes";
+    private string? yesButtonCssClass { get; set; }
+    private string noButtonText { get; set; } = "No";
+    private string? noButtonCssClass { get; set; }
+    private bool isVisible { get; set; } = false;
+
+    private TaskCompletionSource<bool>? taskCompletionSource;
+
+    #region Methods
+
+    public Task<bool> ShowAsync(
+        string? title = null,
+        string? titleHtml = null,
+        string? message = null,
+        string? messageHtml = null)
+    {
+        taskCompletionSource = new TaskCompletionSource<bool>();
+        var task = taskCompletionSource.Task;
+
+        this.title = title;
+        this.titleHtml = titleHtml;
+        this.message = message;
+        this.messageHtml = messageHtml;
+        isVisible = true;
+
+        StateHasChanged();
+
+        return task;
+    }
+
+    private void OnNoClick()
+    {
+        isVisible = false;
+        taskCompletionSource?.SetResult(false);
+    }
+
+    private void OnYesClick()
+    {
+        isVisible = false;
+        taskCompletionSource?.SetResult(true);
+    }
+
+    #endregion
+
+    #region Properties, Indexers
+
+    /// <summary>
+    /// Gets or sets the child content.
+    /// <para>
+    /// Default value is <see langword="null" />.
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Gets or sets the child content.")]
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    protected override string? ClassNames =>
+        BuildClassNames(
+            Class,
+            (BulmaCssClass.Modal, true),
+            (BulmaCssClass.IsActive, isVisible)
+        );
+
+    /// <summary>
+    /// If <see langword="true"/>, modal close button is hidden.
+    /// <para>
+    /// Default value is <see langword="false" />.
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(false)]
+    [Description("If <b>true</b>, modal close button is hidden.")]
+    [Parameter]
+    public bool HideCloseButton { get; set; } = false;
+
+    [Parameter]
+    public string? TitleCssClass { get; set; }
+
+    private string? TitleClassNames =>
+        BuildClassNames(
+            TitleCssClass,
+            (BulmaCssClass.ModalCardTitle, true)
+        );
+
+    [Parameter]
+    public string? BodyCssClass { get; set; }
+
+    private string? BodyClassNames =>
+        BuildClassNames(
+            BodyCssClass,
+            (BulmaCssClass.ModalCardBody, true)
+        );
+
+    [Parameter]
+    public string? FootCssClass { get; set; }
+
+    private string? FootClassNames =>
+        BuildClassNames(
+            FootCssClass,
+            (BulmaCssClass.ModalCardFoot, true)
+        );
+
+    #endregion
+}
