ab08e060781c96b508b3bcd5b2f461b3fecb4dff
Vikram Reddy <gvreddy04@gmail.com>
2025-03-31 00:16:22 +0530

    DateInput updates

--- BlazorExpress.Bulma/Components/Form/DateInput/DateInput.cs
+++ BlazorExpress.Bulma/Components/Form/DateInput/DateInput.cs
@@ -17,15 +17,15 @@ public class DateInput<TValue> : BulmaComponentBase
 
     private FieldIdentifier fieldIdentifier = default!;
 
-    private string formattedMax = default!;
+    private string? maxAsString;
 
-    private string formattedMin = default!;
+    private string? minAsString;
 
-    private string formattedValue = default!;
+    private string? valueAsString;
 
-    private TValue max = default!;
+    private TValue oldMax = default!;
 
-    private TValue min = default!;
+    private TValue oldMin = default!;
 
     private TValue? oldValue;
 
@@ -48,15 +48,16 @@ public class DateInput<TValue> : BulmaComponentBase
         builder.AddAttributeIfNotNullOrWhiteSpace(204, "style", StyleNames);
         if (EnableMaxMin)
         {
-            builder.AddAttributeIfNotNullOrWhiteSpace(205, "max", Max);
-            builder.AddAttributeIfNotNullOrWhiteSpace(206, "min", Min);
+            builder.AddAttributeIfNotNullOrWhiteSpace(205, "max", maxAsString);
+            builder.AddAttributeIfNotNullOrWhiteSpace(206, "min", minAsString);
         }
-        builder.AddAttribute(207, "value", Value);
+        builder.AddAttribute(207, "value", valueAsString);
         builder.AddAttribute(208, "disabled", Disabled);
         builder.AddAttribute(209, "autocomplete", AutoCompleteAsString);
-        builder.AddMultipleAttributes(210, AdditionalAttributes);
-        builder.AddAttribute(211, "onchange", OnChange);
-        builder.AddElementReferenceCapture(212, __inputReference => Element = __inputReference);
+        builder.AddAttribute(210, "step", Step);
+        builder.AddMultipleAttributes(211, AdditionalAttributes);
+        builder.AddAttribute(212, "onchange", OnChange);
+        builder.AddElementReferenceCapture(213, __inputReference => Element = __inputReference);
 
         builder.CloseElement(); // close: input
 
@@ -66,11 +67,72 @@ public class DateInput<TValue> : BulmaComponentBase
 
     protected override void OnInitialized()
     {
+        if (!(typeof(TValue) == typeof(DateOnly)
+              || typeof(TValue) == typeof(DateOnly?)
+              || typeof(TValue) == typeof(DateTime)
+              || typeof(TValue) == typeof(DateTime?)
+             ))
+            throw new InvalidOperationException($"{typeof(TValue)} is not supported.");
+
+        oldMax = Max;
+        oldMin = Min;
+        oldValue = Value;
+
+        // TODO: check min is less than max
+        if (EnableMaxMin)
+        {
+            maxAsString = GetDefaultFormattedValueAsString(Max);
+            minAsString = GetDefaultFormattedValueAsString(Min);
+        }
+
+        valueAsString = GetDefaultFormattedValueAsString(Value);
+
         AdditionalAttributes ??= new Dictionary<string, object>();
 
         fieldIdentifier = FieldIdentifier.Create(ValueExpression!);
 
         base.OnInitialized();
+    }
+
+    protected override Task OnParametersSetAsync()
+    {
+        if (IsFirstRenderComplete)
+        {
+            if (oldMax!.Equals(Max))
+            {
+                oldMax = Max;
+                if (EnableMaxMin)
+                    maxAsString = GetDefaultFormattedValueAsString(Max);
+            }
+
+            if (oldMin!.Equals(Min))
+            {
+                oldMin = Min;
+                if (EnableMaxMin)
+                    minAsString = GetDefaultFormattedValueAsString(Min);
+            }
+
+            if (oldValue!.Equals(Value))
+            {
+                oldValue = Value;
+                valueAsString = GetDefaultFormattedValueAsString(Value);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private string? GetDefaultFormattedValueAsString(TValue value)
+    {
+        if (value == null)
+            return null;
+
+        return value switch
+        {
+            DateOnly dateOnly => dateOnly.ToString(defaultFormat, CultureInfo.CurrentCulture),
+            DateTime dateTime => dateTime.ToString(defaultFormat, CultureInfo.CurrentCulture),
+            _ => throw new InvalidOperationException($"{typeof(TValue)} is not supported.")
+        };
     }
 
     /// <summary>
@@ -89,22 +151,57 @@ public class DateInput<TValue> : BulmaComponentBase
 
     private async Task OnChange(ChangeEventArgs e)
     {
-        var oldValue = Value;
-        var newValue = e.Value?.ToString() ?? string.Empty; // object
+        if (e.Value is string _value)
+        {
+            TValue newValue = default!;
 
-        //await ValueChanged.InvokeAsync(newValue);
+            if (_value != string.Empty)
+                newValue = TryParseValue(_value, out var value) ? value : default!;
 
-        //EditContext?.NotifyFieldChanged(fieldIdentifier);
+            if (ValueChanged.HasDelegate)
+                await ValueChanged.InvokeAsync(newValue);
+
+            EditContext?.NotifyFieldChanged(fieldIdentifier);
+
+            oldValue = newValue;
+        }
     }
 
-    private async Task OnInput(ChangeEventArgs e)
+    private bool TryParseValue(object value, out TValue newValue)
     {
-        var oldValue = Value;
-        var newValue = e.Value?.ToString() ?? string.Empty; // object
+        try
+        {
+            // DateOnly / DateOnly?
+            if (typeof(TValue) == typeof(DateOnly) || typeof(TValue) == typeof(DateOnly?))
+            {
+                if (DateTime.TryParse(value.ToString(), CultureInfo.CurrentCulture, DateTimeStyles.None, out var dt))
+                {
+                    newValue = (TValue)(object)DateOnly.FromDateTime(dt);
 
-        //await ValueChanged.InvokeAsync(newValue);
+                    return true;
+                }
 
-        //EditContext?.NotifyFieldChanged(fieldIdentifier);
+                newValue = default!;
+
+                return false;
+            }
+            // DateTime / DateTime?
+            else if (typeof(TValue) == typeof(DateTime) || typeof(TValue) == typeof(DateTime?))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(DateTime), CultureInfo.CurrentCulture);
+
+                return true;
+            }
+
+            newValue = default!;
+
+            return false;
+        }
+        catch
+        {
+            newValue = default!;
+            return false;
+        }
     }
 
     #endregion
@@ -181,13 +278,24 @@ public class DateInput<TValue> : BulmaComponentBase
     public bool IsRounded { get; set; } = false;
 
     /// <summary>
-    /// Gets or sets the max value.
+    /// Gets or sets the max value. 
+    /// If <see cref="EnableMaxMin" /> is <see langword="true" />, this value will be used.
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Gets or sets the max value. If <code>EnableMaxMin</code> is <b>true</b>, this value will be used.")]
     [Parameter]
-    public string? Max { get; set; }
+    public TValue Max { get; set; } = default!;
 
+    /// <summary>
+    /// Gets or sets the min value.
+    /// If <see cref="EnableMaxMin" /> is <see langword="true" />, this value will be used.
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(null)]
+    [Description("Gets or sets the min value. If <code>EnableMaxMin</code> is <b>true</b>, this value will be used.")]
     [Parameter]
-    public string? Min { get; set; }
+    public TValue Min { get; set; } = default!;
 
     /// <summary>
     /// Gets or sets the size.
