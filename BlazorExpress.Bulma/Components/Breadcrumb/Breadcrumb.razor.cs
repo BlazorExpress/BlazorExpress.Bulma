92c0cc5482515049c1e773dd63ea1e0627948d08
Vikram Reddy <gvreddy04@gmail.com>
2025-04-02 00:30:03 +0530

    Breadcrumb - Component, Demos, Docs - updated

--- BlazorExpress.Bulma/Components/Breadcrumb/Breadcrumb.razor.cs
+++ BlazorExpress.Bulma/Components/Breadcrumb/Breadcrumb.razor.cs
@@ -13,8 +13,22 @@ public partial class Breadcrumb : BulmaComponentBase
     protected override string? ClassNames => 
         BuildClassNames(
             Class, 
-            (BulmaCssClass.Breadcrumb, true)
+            (BulmaCssClass.Breadcrumb, true),
+            (Alignment.ToBreadcrumbAlignmentClass(), Alignment != BreadcrumbAlignment.None),
+            (Size.ToBreadcrumbSizeClass(), Size != BreadcrumbSize.None)
         );
+
+    /// <summary>
+    /// Gets or sets the breadcrumb alignment.
+    /// <para>
+    /// Default value is <see cref="BreadcrumbAlignment.None" />.
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(BreadcrumbAlignment.None)]
+    [Description("Gets or sets the breadcrumb alignment.")]
+    [Parameter]
+    public BreadcrumbAlignment Alignment { get; set; } = BreadcrumbAlignment.None;
 
     /// <summary>
     /// Gets or sets the child content.
@@ -28,6 +42,18 @@ public partial class Breadcrumb : BulmaComponentBase
     [EditorRequired]
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Gets or sets the size.
+    /// <para>
+    /// Default value is <see cref="TextInputSize.None" />.
+    /// </para>
+    /// </summary>
+    [AddedVersion("1.0.0")]
+    [DefaultValue(BreadcrumbSize.None)]
+    [Description("Gets or sets the size.")]
+    [Parameter]
+    public BreadcrumbSize Size { get; set; } = BreadcrumbSize.None;
 
     #endregion
 }
---
+++ BlazorExpress.Bulma/Enums/Size/BreadcrumbSize.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorExpress.Bulma;
+
+/// <summary>
+/// Enum representing the breadcrumb size.
+/// <para>
+/// <see href="https://bulma.io/documentation/components/breadcrumb/#sizes" />
+/// </para>
+/// </summary>
+public enum BreadcrumbSize
+{
+    None,
+    Small,
+    Normal,
+    Medium,
+    Large
+}
