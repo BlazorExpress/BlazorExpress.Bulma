04fac5ef425584e8c3e3cc38234a5f0583731ec3
Vikram Reddy <gvreddy04@gmail.com>
2025-07-13 18:31:21 +0530

    Enhance menu structure and update branding (#27)
    Refactor Blazor components in `BlogMainLayout.razor`, `DemosMainLayout.razor`, and `DocsMainLayout.razor` to replace `<NavLink>` with `<MenuItem>`, introducing `Href` and `Match` properties for improved link handling.
    
    Update button text in `01_A_Manual_Install_06_Snippet.razor` and `02_A_Manual_Install_06_Snippet.razor` to reflect new branding.
    
    Adjust CSS formatting in `Menu.razor.css` for consistency.
    
    Modify rendering logic in `MenuItem.razor` to conditionally wrap `ChildContent` in a `<NavLink>` based on `Href`.
    
    Add `Href` and `Match` properties to the `MenuItem` class in `MenuItem.razor.cs` for enhanced control.
    
    Change `Match` property type in `Link` class from `NavLinkMatch` to `MenuItemMatch` in `Link.cs`.
    
    Update `_Imports.razor` to include additional namespaces for routing and web components.
    
    Introduce new `MenuItemMatch` enum to define menu item matching behavior.

---
+++ BlazorExpress.Bulma/Enums/MenuItemMatch.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorExpress.Bulma;
+
+/// <summary>
+/// Modifies the URL matching behavior for a Microsoft.AspNetCore.Components.Routing.NavLink.
+/// </summary>
+public enum MenuItemMatch
+{
+    /// <summary>
+    /// Specifies that the Microsoft.AspNetCore.Components.Routing.NavLink should be 
+    /// active when it matches any prefix of the current URL.
+    /// </summary>
+    Prefix = 0,
+
+    /// <summary>
+    /// Specifies that the Microsoft.AspNetCore.Components.Routing.NavLink should be
+    /// active when it matches the entire current URL.
+    /// </summary>
+    All = 1
+}
